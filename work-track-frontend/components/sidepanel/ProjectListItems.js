7a8525619d959ed8c0e7db981ed4fa76824e0afa
Bobby BuffaloBoy <novenarii@gmail.com>
2020-10-28 19:19:57 -0500

    rendering issue corrected for project folders in sidePanel

--- work-track-frontend/components/sidepanel/ProjectListItems.js
+++ work-track-frontend/components/sidepanel/ProjectListItems.js
@@ -7,24 +7,36 @@ import ListItemText from "@material-ui/core/ListItemText";
 
 import { client } from "../../api/client";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  listText: {
+    color: "white",
+    fontSize: "10px",
+  },
+}));
 
 export default function ProjectListItems() {
   const classes = useStyles();
-  const [projects, setProjects] = useState();
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     client("projects").then((data) => {
-      const list = document.getElementById("projectsList");
-
-      for (let i = 0; i < data.length; i++) {
-        if (data[i].user_id === parseInt(localStorage.id)) {
-          console.log(data[i].title);
-          list.innerHTML += `<div class="MuiButtonBase-root MuiListItem-root MuiListItem-gutters MuiListItem-button" tabindex="0" role="button" aria-disabled="false"><div class="MuiListItemText-root"><span class="MuiTypography-root MuiListItemText-primary makeStyles-listText-4 MuiTypography-body1 MuiTypography-displayBlock">${data[i].title}</span></div><span class="MuiTouchRipple-root"></span></div>`;
-        }
-      }
+      setProjects(data);
     });
   }, []);
 
-  return <List component="div" disablePadding id="projectsList"></List>;
+  return (
+    <List component="div" disablePadding id="projectsList">
+      {projects.map((p) => {
+        return (
+          <ListItem button className={classes.nested}>
+            <ListItemText
+              key={p.title}
+              primary={p.title}
+              classes={{ primary: classes.listText }}
+            />
+          </ListItem>
+        );
+      })}
+    </List>
+  );
 }
